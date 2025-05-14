c0409300ee54308ea3e57d7dbc7de53b6a0c4dbc
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-05-14 16:49:24 +0800

    update all

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request
 from google import genai
+import openai
 import markdown
 import os
 
 #Load environment variables from .env file
 # from dotenv import load_dotenv
 # load_dotenv()
-# api_key = os.environ.get("GOOGLE_API")
+# genmini_api_key = os.environ.get("GOOGLE_API")
+# openai_api_key = os.environ.get("OPENAI_API")
 #print("API Key:", api_key)  # Debugging line to check if the API key is loaded correctly
 
-api_key = os.getenv('GOOGLE_API')
+genmini_api_key = os.getenv('GOOGLE_API')
+openai_api_key = os.getenv('OPENAI_API')
 
-client = genai.Client(api_key=api_key)
+genmini_client = genai.Client(api_key=genmini_api_key)
+genmini_model = "gemini-2.0-flash"
+
+openai_client = openai.OpenAI(api_key=openai_api_key)
+openai_model = "gpt-4o"
+
 
 app = Flask(__name__)
-model = "gemini-2.0-flash"
-
 @app.route("/",methods=["GET","POST"])
 def index():
         return(render_template("index.html"))
@@ -27,12 +33,27 @@ def gemini():
 @app.route("/gemini_reply",methods=["GET","POST"])
 def gemini_reply():
     q = request.form.get("q")
-    r = client.models.generate_content(model=model,contents=q)
+    r = genmini_client.models.generate_content(model=genmini_model,contents=q)
     r_html = markdown.markdown(
             r.text,
             extensions=["fenced_code", "codehilite"]  
     )
     return(render_template("gemini_reply.html",r=r_html))
+
+@app.route("/openai",methods=["GET","POST"])
+def openai():
+    return(render_template("openai.html"))
+
+
+@app.route("/openai_reply",methods=["GET","POST"])
+def openai_reply():
+    q = request.form.get("q")
+    r = openai_client.chat.completions.create(model=openai_model,messages=[{"role": "user", "content": q}],)
+    r_html = markdown.markdown(
+            r.choices[0].message.content,
+            extensions=["fenced_code", "codehilite"]  
+    )
+    return(render_template("openai_reply.html",r=r_html))
 
 if __name__ == "__main__":
     app.run()
